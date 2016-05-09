c8883268b76befd5bb875f482c072c8654660be5
flauc <filip.lauc93@gmail.com>
2016-05-09 22:24:26 +0200

    chore: switched to an inline template

--- public/app/app.component.ts
+++ public/app/app.component.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
     selector: 'app',
-    templateUrl: 'app/app.html',
+    template: `<p>Hello World</p>`,
 })
 export class AppComponent {
     constructor() {}
