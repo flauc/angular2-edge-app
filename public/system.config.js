8c4fff9cec1ee724764d30a2ab3ba336783fd270
flauc <filip.lauc93@gmail.com>
2016-05-21 16:42:42 +0200

    chore: renamed boot.ts to main.ts

--- public/system.config.js
+++ public/system.config.js
@@ -9,7 +9,7 @@
     },
 
     packages = {
-        'app': { main: 'boot.js',  defaultExtension: 'js' },
+        'app': { main: 'main.js',  defaultExtension: 'js' },
         'rxjs': { defaultExtension: 'js' },
         'angular2-in-memory-web-api': { defaultExtension: 'js' }
     },
