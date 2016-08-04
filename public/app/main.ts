32fcc4df39351694de6522df978daef54de7b9e4
flauc <filip.lauc93@gmail.com>
2016-08-04 11:37:14 +0200

    chore: Implemented new forms

--- public/app/main.ts
+++ public/app/main.ts
@@ -5,13 +5,17 @@ import {ApiService} from './common/services/api.service'
 import {DataService} from './common/services/data.service'
 import {UserStoreService} from './common/services/user-store.service'
 import {routerProviders} from './app.routes';
+import {provideForms, disableDeprecatedForms} from '@angular/forms';
 
 bootstrap(AppComponent, [
     HTTP_PROVIDERS,
 
     routerProviders,
 
+    disableDeprecatedForms(),
+    provideForms(),
+
     UserStoreService,
     ApiService,
     DataService
-]);
+]).catch((err: any) => console.error(err));
